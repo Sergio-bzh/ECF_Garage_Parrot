45cae143f7187f82eeb58db06b696d0ffaa66b50
sergio-nunez <sergio.nunez@laposte.net>
2023-11-13 16:26:15 +0100

    Config: add php.ini file and install Sass, Postcss, Bootstrap, Popperjs

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+        // include whatever plugin you want
+        // but make sure you install these via yarn or npm !
+
+        // add browser list config to package.json (see below)
+        autoprefixer: ()
+    }
+}
