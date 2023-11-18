2c0a7a6d1349c564db71cc8c86e7334273d1bc61
sergio-nunez <sergio.nunez@laposte.net>
2023-11-18 18:14:51 +0100

    Config: project packages updated via composer

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,9 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 
-// import {Tooltip, Toast, Popover} from "bootstrap";
+import {Tooltip, Toast, Popover} from "bootstrap";
 import 'bootstrap';
 
 // The code here below makes all pics in images folder to be used in twig templates
-/*const imagesContext = require.context('./images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
-imagesContext.keys().forEach(imagesContext);*/
+const imagesContext = require.context('./images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
+imagesContext.keys().forEach(imagesContext);
