cfb07ddf326811b652b4cb342d4f863ad5481206
sergio-nunez <sergio.nunez@laposte.net>
2024-02-04 22:52:36 +0100

    Content: add images in vehicles pages

--- src/Repository/VehicleRepository.php
+++ src/Repository/VehicleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Vehicle;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,6 +20,36 @@ class VehicleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Vehicle::class);
+    }
+
+    public function findVehiclesPaginated(int $page, int $limit): array
+    {
+        $limit = abs($limit);
+
+        $result = [];
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+            ->select('vehicles')
+            ->from('App\Entity\Vehicle', 'vehicles')
+            ->setMaxResults($limit)
+            ->setFirstResult($page * $limit - $limit);
+
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        if(empty($data))
+        {
+            return $result;
+        }
+
+        $pages = ceil($paginator->count() / $limit);
+
+        $result['data']     = $data;
+        $result['pages']    = $pages;
+        $result['page']     = $page;
+        $result['limit']    = $limit;
+
+        return $result;
     }
 
 //    /**
